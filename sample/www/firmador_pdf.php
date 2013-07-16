d4d1f7bf9e8607679e01741e3d24659ebaa95124
seba <seba@65e1b02d-848e-4964-89f3-a9fece5adf98>
2013-07-16 19:43:04 +0000

    Firmador: ejemplo utilizando sesion de php

--- sample/www/firmador_pdf.php
+++ sample/www/firmador_pdf.php
@@ -2,19 +2,27 @@
 
 class firmador_pdf
 {
-
+	protected $sesion_handler = 'php';
     protected $db = null;
     protected $sesion;
-    
-  
+	
    function set_guardar_sesion_en_db($conexion)
    {
        $this->db = $conexion;
+	   $this->sesion_handler = 'db';
    }
    
    function set_guardar_sesion_en_xml()
    {
-       $this->db = null;
+	   $this->sesion_handler = 'xml';
+   }
+   
+   function set_guardar_sesion_en_php()
+   {
+	   $this->sesion_handler = 'php';
+	   if(! isset($_SESSION)) {
+		   session_start();
+	   }
    }
    
    
@@ -36,6 +44,28 @@ class firmador_pdf
         return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port;
    }
    
+      
+   function generar_applet($url_jar, $url_descarga, $url_subir, $motivo, $width = '700', $height = '310')
+   {
+        $sesion = $this->generar_sesion();
+		$param_cookie = "";
+		if ($this->sesion_handler == 'php') {
+			$cookie = session_name()."=".session_id();
+			$param_cookie = "<param  name='COOKIE' value='$cookie' />";
+		}
+?>
+        <applet  code="ar/gob/onti/firmador/view/FirmaApplet" 	 
+           archive="<?php echo $url_jar; ?>"  width="<?php echo $width; ?>"	height="<?php echo $height; ?>" >
+         <param  name="URL_DESCARGA"	 value="<?php echo $url_descarga; ?>" >
+         <param  name="URL_SUBIR"	value="<?php echo $url_subir; ?>">
+         <param  name="MOTIVO"  value="<?php echo $motivo; ?>">
+         <param  name="CODIGO"  value="<?php echo $sesion; ?>" />
+         <param name="PREGUNTAS" value='{ "preguntasRespuestas": []}' />
+		 <param name='codebase_lookup' value='false' >
+		 <?php echo $param_cookie; ?>
+        </applet>
+<?php
+   }
    
    //-----------------------------------------
    //--- MANEJO DE SESION
@@ -47,7 +77,7 @@ class firmador_pdf
 			//Agregar un token a un archivo xml de sesiones (para evitar usar una BD en el ejemplo)
 			$this->sesion = hash('sha256', uniqid(mt_rand(), true));
 
-			if ($this->db == null) {
+			if ($this->sesion_handler == 'xml') {
 				$archivo_sesiones = dirname(dirname(__FILE__)).'/sesiones.xml';
 				if (file_exists($archivo_sesiones)) {
 					$xml = simplexml_load_file($archivo_sesiones);
@@ -63,13 +93,15 @@ XML;
 				if (!file_put_contents($archivo_sesiones, $xml->asXML())) {
 					die("El usuario apache debe tener permisos sobre esta carpeta");
 				}
-			 } else {
+			 } elseif ($this->sesion_handler == 'db') {
 				//BD
 				 $this->generar_tabla();
 
 				//Insertar nueva sesión
 				$sql = "INSERT INTO firmador_pdf_sesion (sesion) VALUES (".$this->db->quote($this->sesion).")";
 				$this->db->exec($sql);
+			 } elseif ($this->sesion_handler == 'php') {
+				$_SESSION['firmador_pdf_token'] = $this->sesion;
 			 }
 		}
         return $this->sesion;
@@ -77,7 +109,7 @@ XML;
      
      function validar_sesion($sesion)
      {
-        if ($this->db == null) {
+        if ($this->sesion_handler == 'xml') {
             $archivo_sesiones = dirname(dirname(__FILE__)).'/sesiones.xml';
             if (file_exists($archivo_sesiones)) {
                 $xml = simplexml_load_file($archivo_sesiones);
@@ -90,7 +122,7 @@ XML;
                 }
             }
             return false;
-        } else {
+        } elseif ($this->sesion_handler == 'db') {
             //BD
             $this->generar_tabla();
             
@@ -102,7 +134,9 @@ XML;
             $sql = "SELECT count(*) from firmador_pdf_sesion where sesion = ".$this->db->quote($sesion);
             $rs = $this->db->query($sql);
             return !empty($rs);
-        }
+        } elseif ($this->sesion_handler == 'php') {
+			return isset($_SESSION['firmador_pdf_token']) && $_SESSION['firmador_pdf_token'] === (string) $sesion;
+		}
      }
      
      function generar_tabla()
--- sample/www/index.php
+++ sample/www/index.php
@@ -7,26 +7,26 @@ $firmador = new firmador_pdf();
 //$firmador->set_guardar_sesion_en_xml();
 
 //-- Guardar sesion en BD
-$nombre_base = "toba_trunk";
-$dbh = new PDO("pgsql:host=localhost;dbname=$nombre_base", "postgres", "");
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set Errorhandling to Exception
-$firmador->set_guardar_sesion_en_db($dbh);
+//$nombre_base = "toba_trunk";
+//$dbh = new PDO("pgsql:host=localhost;dbname=$nombre_base", "postgres", "");
+//$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set Errorhandling to Exception
+//$firmador->set_guardar_sesion_en_db($dbh);
+
+//-- Compartir sesion de PHP con el applet
+$firmador->set_guardar_sesion_en_php();
 
 
+//-- Enviar applet
 if (! isset($_GET['accion'])) {
-        $url_actual = $firmador->get_url_base_actual(). $_SERVER['REQUEST_URI'];
-        $sesion = $firmador->generar_sesion();
-?>
-        <applet  code="ar/gob/onti/firmador/view/FirmaApplet" 	 
-           archive="firmador.jar"  width="700"	height="310" >
-         <param  name="URL_DESCARGA"	 value="<?php echo $url_actual; ?>?accion=descargar" >
-         <param  name="URL_SUBIR"	value="<?php echo $url_actual; ?>?accion=subir">
-         <param  name="MOTIVO"  value="Insertar motivo de la firma">
-         <param  name="CODIGO"  value="<?php echo $sesion; ?>" />
-         <param name="PREGUNTAS" value='{ "preguntasRespuestas": []}' />
-        </applet>
-<?php
-     die;
+	$url_actual = $firmador->get_url_base_actual(). $_SERVER['REQUEST_URI'];
+	$firmador->generar_applet("firmador.jar", 
+								$url_actual."?accion=descargar",
+								$url_actual."?accion=subir",
+								"Motivo de firma",
+								700,
+								310
+			);
+	die;
 }
 
 //-- DESCARGAR
@@ -42,9 +42,9 @@ if ($_GET['accion'] == 'descargar') {
 	//Enviar PDF
 	$firmador->enviar_headers_pdf();
         
-        $file = dirname(dirname(__FILE__)).'/docOriginal.pdf';
-        $fd = fopen($file,'r');
-        fpassthru($fd);
+	$file = dirname(dirname(__FILE__)).'/docOriginal.pdf';
+	$fd = fopen($file,'r');
+	fpassthru($fd);
 	die;
 }
 
